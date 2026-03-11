94f6a28d50c3d32935005a49953c14ca0d059cec
Breno Rodrigues Freitas <brenofreitas12376@gmail.com>
2026-03-10 21:19:24 -0300

    atualizado para adição de imagens e acesso por streamlit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import requests
 import os
 from dotenv import load_dotenv
@@ -5,7 +6,12 @@ from cat_engine import choose_cat
 
 load_dotenv()
 
-API_KEY=os.getenv("API_KEY")
+API_KEY = os.getenv("API_KEY")
+
+st.title("🐱 WeatherCat")
+st.write("Weather and cat mood based on your city")
+
+city = st.text_input("Enter a city")
 
 def get_weather(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
@@ -18,23 +24,25 @@ def get_weather(city):
         return None
 
 
-city = input("Enter the city name: ")
+if city:
 
-data = get_weather(city)
+    data = get_weather(city)
 
-if data:
+    if data:
 
-    temperature = data["main"]["temp"]
-    weather = data["weather"][0]["description"]
-    humidity = data["main"]["humidity"]
+        temperature = data["main"]["temp"]
+        weather = data["weather"][0]["description"]
+        humidity = data["main"]["humidity"]
 
-    cat = choose_cat(temperature, weather)
+        cat = choose_cat(temperature, weather)
 
-    print(f"\nWeather in {city}")
-    print(f"Temperature: {temperature}°C")
-    print(f"Condition: {weather}")
-    print(f"Humidity: {humidity}%")
-    print(f"Cat mood: {cat}")
+        st.subheader(f"Weather in {city}")
+        st.write(f"Temperature: {temperature} °C")
+        st.write(f"Condition: {weather}")
+        st.write(f"Humidity: {humidity}%")
 
-else:
-    print("Error fetching weather data.")
+        st.subheader("Cat mood")
+        st.write(cat)
+
+    else:
+        st.write("City not found.")
